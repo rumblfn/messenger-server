06903bdf7d9fee4f1deb27a723508a8d68808ce8
Shamil <toshamilgis@gmail.com>
2022-07-01 03:22:25 +0300

    auth

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Client } = require("pg")
+require("dotenv").config();
+
+const pool = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    } 
+})
+
+module.exports = pool
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const {Server} = require("socket.io")
 const app = express();
 const cors = require("cors")
 const authRouter = require("./routers/authRouter")
-
+const session = require("express-session")
+require("dotenv").config();
 const server = require("http").createServer(app)
 
 const io = new Server(server, {
@@ -20,7 +21,18 @@ app.use(cors({
     credentials: true,
 }))
 app.use(express.json());
-
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    credentials: true,
+    name: "sid",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.ENVIRONMENT === "production",
+        httpOnly: true,
+        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+    }
+}))
 app.use("/auth", authRouter)
 
 app.get('/', (req, res) => {
