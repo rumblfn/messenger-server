280a26cc3dee8666fc363f2f110a9503051b6c5a
Shamil <toshamilgis@gmail.com>
2022-07-20 13:56:26 +0300

    app

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const server = require("http").createServer(app)
 let socketList = {};
 const socketIdToRoom = {}
 
+const users = {};
+const socketToRoom = {};
+
 const {
     sessionMiddleware,
     corsConfig,
@@ -84,7 +87,6 @@ io.on("connect", socket => {
         onDisconnect(socket)
 
         const roomId = socketIdToRoom[socket.id]
-        
         delete socketList[socket.id];
         io
             .to(roomId)
@@ -92,6 +94,14 @@ io.on("connect", socket => {
                 userId: socket.id
             });
         socket.leave(roomId)
+
+
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
     })
 
     socket.on("chatMessages", userid => chatMessages(socket, userid))
@@ -113,96 +123,6 @@ io.on("connect", socket => {
     socket.on('answerCall', data => {
         socket.to(data.to).emit("callAccepted", data.signal)
     })
-
-
-    // new rooms
-
-    socket.on('BE-check-user', ({
-        roomId
-    }) => {
-        let error = false;
-
-        const clients = io.sockets.adapter.rooms[roomId];
-
-        if (clients) {
-            clients.forEach((client) => {
-                if (socketList[client] == socket.user.username) {
-                    error = true;
-                }
-            });
-        } else {
-            socket.emit('FE-error-user-exist', {
-                error,
-                username: socket.user.username,
-                roomName: roomId
-            });
-        }
-    });
-
-    socket.on('BE-leave-room', ({
-        roomId
-    }) => {
-        delete socketList[socket.id];
-        io
-            .to(roomId)
-            .emit('FE-user-leave', {
-                userId: socket.id
-            });
-        socket.leave(roomId);
-    });
-
-    socket.on('BE-join-room', ({
-        roomId
-    }) => {
-        const userName = socket.user.username
-        // Socket Join RoomName
-        socket.join(roomId);
-        socketIdToRoom[socket.id] = roomId
-        socketList[socket.id] = {
-            userName,
-            video: true,
-            audio: true
-        };
-
-        console.log(socketList)
-
-        // Set User List
-
-        const clients = io.sockets.adapter.rooms.get(roomId) || []
-        try {
-            const users = [];
-            clients.forEach((client) => {
-                users.push({
-                    userId: client,
-                    info: socketList[client]
-                });
-            });
-            io.to(roomId).emit('FE-user-join', users);
-        } catch (e) {
-            io.in(roomId).emit('FE-error-user-exist', {
-                err: true
-            });
-        }
-    });
-
-    socket.on('BE-call-user', ({
-        userToCall,
-        from,
-        signal
-    }) => {
-        socket.to(userToCall).emit('FE-receive-call', {
-            signal,
-            from,
-            info: socketList[socket.id],
-        });
-    });
-
-    socket.on('BE-accept-call', ({ signal, to }) => {
-        socket.to(to).emit('FE-call-accepted', {
-          signal,
-          answerId: socket.id,
-        });
-    });
 
 });
 
